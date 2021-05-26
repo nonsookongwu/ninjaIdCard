19f05dc9dfc1a1b618aab4c855ebb4481d0bd04c
nonsookongwu <60108641+nonsookongwu@users.noreply.github.com>
2021-05-26 16:12:30 +0100

    Stateless widgets ends here

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:NinjaCard() ,
+    home: NinjaCard(),
   ));
 }
 
@@ -19,20 +20,29 @@ class NinjaCard extends StatelessWidget {
         backgroundColor: Colors.blue[700],
         elevation: 0.0,
       ),
-      body:Padding(
+      body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('assets/nonso.jpg'),
+              radius: 50.0,
+            ),
+            SizedBox(height: 10.0,),
+            Divider(
+              height: 60.0,
+              color: Colors.white,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(
-                    Icons.face
+                Icon(Icons.face,  color: Colors.white54,),
+                SizedBox(
+                  width: 2.0,
                 ),
-                SizedBox( width: 2.0,),
                 Text(
-                    'NAME:',
+                  'NAME:',
                   style: TextStyle(
                     color: Colors.white,
                     letterSpacing: 2.0,
@@ -46,20 +56,21 @@ class NinjaCard extends StatelessWidget {
             Text(
               'Okongwu Nonso',
               style: TextStyle(
-                color: Colors.white54,
-                letterSpacing: 2.0,
-                fontSize: 22.0,
-                fontWeight: FontWeight.bold
-              ),
+                  color: Colors.white54,
+                  letterSpacing: 2.0,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 30.0,),
+            SizedBox(
+              height: 30.0,
+            ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center ,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(
-                    Icons.work
+                Icon(Icons.work,  color: Colors.white54,),
+                SizedBox(
+                  width: 5.0,
                 ),
-                SizedBox(width: 5.0,),
                 Text(
                   'JOB DESCRIPTION:',
                   style: TextStyle(
@@ -78,16 +89,28 @@ class NinjaCard extends StatelessWidget {
                   color: Colors.white54,
                   letterSpacing: 2.0,
                   fontSize: 22.0,
-                  fontWeight: FontWeight.bold
-              ),
+                  fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 30.0,),
-            Text(
-              'E-MAIL:',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.email,
+                  color: Colors.white54,
+                ),
+                SizedBox(
+                  width: 5.0,
+                ),
+                Text(
+                  'E-MAIL:',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+              ],
             ),
             SizedBox(
               height: 5.0,
@@ -98,16 +121,29 @@ class NinjaCard extends StatelessWidget {
                   color: Colors.white54,
                   letterSpacing: 2.0,
                   fontSize: 22.0,
-                  fontWeight: FontWeight.bold
-              ),
+                  fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 30.0,),
-            Text(
-              'PHONE:',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                    Icons.phone,
+                  color: Colors.white54,
+                ),
+                SizedBox(
+                  width: 5.0,
+                ),
+                Text(
+                  'PHONE:',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                  ),
+                ),
+              ],
             ),
             SizedBox(
               height: 5.0,
@@ -118,14 +154,11 @@ class NinjaCard extends StatelessWidget {
                   color: Colors.white54,
                   letterSpacing: 2.0,
                   fontSize: 22.0,
-                  fontWeight: FontWeight.bold
-              ),
+                  fontWeight: FontWeight.bold),
             ),
           ],
         ),
-      ) ,
+      ),
     );
   }
 }
-
-
